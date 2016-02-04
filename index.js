d025b7f2141820543d89d15a6f8fee99bed3d11b
inikulin <ifaaan@gmail.com>
2016-02-04 17:48:24 +0300

    Fix highlighting and frame lookup. Change API.

--- index.js
+++ index.js
@@ -4,50 +4,60 @@ var jsTokensRe     = require('js-tokens');
 var leftPad        = require('left-pad');
 var isReservedWord = require('esutils').keyword.isReservedWordES6;
 
-// Decorators
-exports.decorators = {
-    default: {
-        line: function (num, base, src) {
-            var prefix = base ? '>' : ' ';
-
-            return prefix + num + ' |' + src + '\n';
-        }
-    }
-};
-
+var NEWLINE = /\r\n|[\n\r\u2028\u2029]/;
 
 // Internals
-function getFrameLines (filename, baseLineIdx, frameSize) {
-    var content          = readFile(filename);
-    var lines            = content.split(/\r?\n/g);
+function highlight (src, decorator) {
+    return src.replace(jsTokensRe, function (value) {
+        var token = jsTokensRe.matchToToken.apply(jsTokensRe, arguments);
+
+        if (token.type === 'name' && isReservedWord(value, true))
+            token.type = 'keyword';
+
+        if (decorator[token.type]) {
+            return value
+                .split(NEWLINE)
+                .map(function (str) {
+                    return decorator[token.type](str);
+                })
+                .join('\n');
+        }
+
+        return value;
+    });
+}
+
+function getFrameLines (filename, baseLineIdx, decorator, frameSize) {
+    var content          = readFile(filename).toString();
+    var lines            = content.split(NEWLINE);
     var startLineIdx     = Math.max(0, baseLineIdx - frameSize);
     var endLineIdx       = Math.min(lines.length - 1, baseLineIdx + frameSize);
     var maxLineNumDigits = 0;
     var frameLines       = [];
+    var sources          = [];
+    var lineNumbers      = [];
 
     for (var i = startLineIdx; i <= endLineIdx; i++) {
         var num = String(i);
 
         maxLineNumDigits = Math.max(maxLineNumDigits, num.length);
 
-        frameLines.push({ num: num, base: i === baseLineIdx, src: lines[i] });
+        lineNumbers.push(num);
+        sources.push(lines[i]);
     }
+
+    sources = highlight(sources.join('\n'), decorator).split(NEWLINE);
 
     frameLines.forEach(function (line) {
         line.num = leftPad(line.num, maxLineNumDigits);
     });
 
-    return frameLines;
-}
-
-function highlight (src, decorator) {
-    return src.replace(jsTokensRe, function () {
-        var token = jsTokensRe.matchToToken.apply(jsTokensRe, arguments);
-
-        if (token.type === 'name' && isReservedWord(token.value, true))
-            token.type = 'keyword';
-
-        return decorator[token.type] ? decorator[token.type](token.value) : token.value;
+    return sources.map(function (src, idx) {
+        return {
+            src:  src,
+            num:  leftPad(lineNumbers[idx], maxLineNumDigits),
+            base: idx === baseLineIdx - startLineIdx
+        };
     });
 }
 
@@ -55,22 +65,42 @@ function createSourceFrame (filename, baseLineIdx, decorator, frameSize) {
     decorator = decorator || exports.decorators.default;
     frameSize = frameSize || 2;
 
-    return getFrameLines(filename, baseLineIdx, frameSize)
+    return getFrameLines(filename, baseLineIdx, decorator, frameSize)
         .reduce(function (sourceFrame, line) {
-            return sourceFrame + decorator.line(line.num, line.base, highlight(line.src));
+            return sourceFrame + decorator.line(line.num, line.base, line.src);
         }, '');
 }
 
 
 // API
-exports.forFn = function (fnName, decorator, frameSize) {
-    // TODO
+module.exports = function (fnName, typeName, decorator, frameSize) {
+    if (typeName && fnName === 'constructor')
+        fnName = typeName;
+
+    var stackFrames = callsite();
+
+    // NOTE: remove lib API call frame
+    stackFrames.shift();
+
+    for (var i = 0; i < stackFrames.length; i++) {
+        var frame        = stackFrames[i];
+        var nextFrame    = stackFrames[i + 1];
+        var isMemberCall = typeName && frame.getMethodName() === fnName && frame.getTypeName() === typeName;
+
+        if ((isMemberCall || frame.getFunctionName() === fnName) && nextFrame)
+            return createSourceFrame(nextFrame.getFileName(), nextFrame.getLineNumber() - 1, decorator, frameSize);
+    }
+
+    return null;
 };
 
-exports.forMemberFn = function (typeName, fnName, decorator, frameSize) {
-    // TODO
-};
+// Decorators
+exports.decorators = {
+    default: {
+        line: function (num, base, src) {
+            var prefix = base ? '> ' : '  ';
 
-exports.forStackFrame = function (idx, decorator, frameSize) {
-    // TODO
+            return prefix + num + ' |' + src + '\n';
+        }
+    }
 };
