bc388b2cb8132a11534d0656da7455e70acf0f6a
inikulin <ifaaan@gmail.com>
2016-02-11 16:39:26 +0300

    Default renderer test

---
+++ test/data/expected-default-win.js
@@ -0,0 +1,72 @@
+module.exports = [
+    '    90 |    \u001b[90m\u001b[1m// ***************************************************\u001b[22m\u001b[39m\n' +
+    '    91 |    \u001b[90m\u001b[1m// ***************************************************\u001b[22m\u001b[39m\n' +
+    '    92 |\n' + '    93 |    \u001b[90m\u001b[1m/* Multiline\u001b[22m\u001b[39m\n' +
+    '    94 |\u001b[90m\u001b[1m         comment*/\u001b[22m\u001b[39m\n' +
+    '\u001b[41m >  95 \u001b[49m|    \u001b[36mvar\u001b[39m testClass \u001b[90m=\u001b[39m \u001b[36mnew\u001b[39m TestClass\u001b[90m(\u001b[39m\u001b[90m)\u001b[39m\u001b[90m;\u001b[39m\n' +
+    '    96 |\n' +
+    '    97 |    testClass\u001b[90m.\u001b[39msomeFunc\u001b[90m(\u001b[39m\u001b[90m)\u001b[39m\u001b[90m;\u001b[39m\n' +
+    '    98 |    TestClass\u001b[90m.\u001b[39mstaticFunc\u001b[90m(\u001b[39m\u001b[90m)\u001b[39m\u001b[90m;\u001b[39m\n' +
+    '    99 |\n' +
+    '   100 |    obj\u001b[90m.\u001b[39mfunc\u001b[90m(\u001b[39m\u001b[90m)\u001b[39m\u001b[90m;\u001b[39m',
+
+
+    '    92 |\n' + '    93 |    \u001b[90m\u001b[1m/* Multiline\u001b[22m\u001b[39m\n' +
+    '    94 |\u001b[90m\u001b[1m         comment*/\u001b[22m\u001b[39m\n' +
+    '    95 |    \u001b[36mvar\u001b[39m testClass \u001b[90m=\u001b[39m \u001b[36mnew\u001b[39m TestClass\u001b[90m(\u001b[39m\u001b[90m)\u001b[39m\u001b[90m;\u001b[39m\n' +
+    '    96 |\n' +
+    '\u001b[41m >  97 \u001b[49m|    testClass\u001b[90m.\u001b[39msomeFunc\u001b[90m(\u001b[39m\u001b[90m)\u001b[39m\u001b[90m;\u001b[39m\n' +
+    '    98 |    TestClass\u001b[90m.\u001b[39mstaticFunc\u001b[90m(\u001b[39m\u001b[90m)\u001b[39m\u001b[90m;\u001b[39m\n' +
+    '    99 |\n' +
+    '   100 |    obj\u001b[90m.\u001b[39mfunc\u001b[90m(\u001b[39m\u001b[90m)\u001b[39m\u001b[90m;\u001b[39m\n' +
+    '   101 |\n' +
+    '   102 |    \u001b[90m(\u001b[39m\u001b[36mfunction\u001b[39m \u001b[90m(\u001b[39m\u001b[90m)\u001b[39m \u001b[90m{\u001b[39m',
+
+
+    '    93 |    \u001b[90m\u001b[1m/* Multiline\u001b[22m\u001b[39m\n' +
+    '    94 |\u001b[90m\u001b[1m         comment*/\u001b[22m\u001b[39m\n' +
+    '    95 |    \u001b[36mvar\u001b[39m testClass \u001b[90m=\u001b[39m \u001b[36mnew\u001b[39m TestClass\u001b[90m(\u001b[39m\u001b[90m)\u001b[39m\u001b[90m;\u001b[39m\n' +
+    '    96 |\n' +
+    '    97 |    testClass\u001b[90m.\u001b[39msomeFunc\u001b[90m(\u001b[39m\u001b[90m)\u001b[39m\u001b[90m;\u001b[39m\n' +
+    '\u001b[41m >  98 \u001b[49m|    TestClass\u001b[90m.\u001b[39mstaticFunc\u001b[90m(\u001b[39m\u001b[90m)\u001b[39m\u001b[90m;\u001b[39m\n' +
+    '    99 |\n' +
+    '   100 |    obj\u001b[90m.\u001b[39mfunc\u001b[90m(\u001b[39m\u001b[90m)\u001b[39m\u001b[90m;\u001b[39m\n' +
+    '   101 |\n' +
+    '   102 |    \u001b[90m(\u001b[39m\u001b[36mfunction\u001b[39m \u001b[90m(\u001b[39m\u001b[90m)\u001b[39m \u001b[90m{\u001b[39m\n' +
+    '   103 |        regularFunc2\u001b[90m(\u001b[39m\u001b[90m)\u001b[39m\u001b[90m;\u001b[39m',
+
+
+    '    95 |    \u001b[36mvar\u001b[39m testClass \u001b[90m=\u001b[39m \u001b[36mnew\u001b[39m TestClass\u001b[90m(\u001b[39m\u001b[90m)\u001b[39m\u001b[90m;\u001b[39m\n' +
+    '    96 |\n' +
+    '    97 |    testClass\u001b[90m.\u001b[39msomeFunc\u001b[90m(\u001b[39m\u001b[90m)\u001b[39m\u001b[90m;\u001b[39m\n' +
+    '    98 |    TestClass\u001b[90m.\u001b[39mstaticFunc\u001b[90m(\u001b[39m\u001b[90m)\u001b[39m\u001b[90m;\u001b[39m\n' +
+    '    99 |\n' +
+    '\u001b[41m > 100 \u001b[49m|    obj\u001b[90m.\u001b[39mfunc\u001b[90m(\u001b[39m\u001b[90m)\u001b[39m\u001b[90m;\u001b[39m\n' +
+    '   101 |\n' +
+    '   102 |    \u001b[90m(\u001b[39m\u001b[36mfunction\u001b[39m \u001b[90m(\u001b[39m\u001b[90m)\u001b[39m \u001b[90m{\u001b[39m\n' +
+    '   103 |        regularFunc2\u001b[90m(\u001b[39m\u001b[90m)\u001b[39m\u001b[90m;\u001b[39m\n' +
+    '   104 |    \u001b[90m}\u001b[39m\u001b[90m)\u001b[39m\u001b[90m(\u001b[39m\u001b[90m)\u001b[39m\u001b[90m;\u001b[39m\n' +
+    '   105 |',
+
+
+    '    98 |    TestClass\u001b[90m.\u001b[39mstaticFunc\u001b[90m(\u001b[39m\u001b[90m)\u001b[39m\u001b[90m;\u001b[39m\n' +
+    '    99 |\n' +
+    '   100 |    obj\u001b[90m.\u001b[39mfunc\u001b[90m(\u001b[39m\u001b[90m)\u001b[39m\u001b[90m;\u001b[39m\n' +
+    '   101 |\n' +
+    '   102 |    \u001b[90m(\u001b[39m\u001b[36mfunction\u001b[39m \u001b[90m(\u001b[39m\u001b[90m)\u001b[39m \u001b[90m{\u001b[39m\n' +
+    '\u001b[41m > 103 \u001b[49m|        regularFunc2\u001b[90m(\u001b[39m\u001b[90m)\u001b[39m\u001b[90m;\u001b[39m\n' +
+    '   104 |    \u001b[90m}\u001b[39m\u001b[90m)\u001b[39m\u001b[90m(\u001b[39m\u001b[90m)\u001b[39m\u001b[90m;\u001b[39m\n' +
+    '   105 |\n' +
+    '   106 |    \u001b[90m[\u001b[39m\u001b[32m\'test\'\u001b[39m\u001b[90m]\u001b[39m\u001b[90m.\u001b[39mforEach\u001b[90m(\u001b[39mregularFunc2\u001b[90m)\u001b[39m\u001b[90m;\u001b[39m\n' +
+    '   107 |\n' + '   108 |    \u001b[36mreturn\u001b[39m frames\u001b[90m;\u001b[39m',
+
+
+    '   101 |\n' +
+    '   102 |    \u001b[90m(\u001b[39m\u001b[36mfunction\u001b[39m \u001b[90m(\u001b[39m\u001b[90m)\u001b[39m \u001b[90m{\u001b[39m\n' +
+    '   103 |        regularFunc2\u001b[90m(\u001b[39m\u001b[90m)\u001b[39m\u001b[90m;\u001b[39m\n' +
+    '   104 |    \u001b[90m}\u001b[39m\u001b[90m)\u001b[39m\u001b[90m(\u001b[39m\u001b[90m)\u001b[39m\u001b[90m;\u001b[39m\n' +
+    '   105 |\n' +
+    '\u001b[41m > 106 \u001b[49m|    \u001b[90m[\u001b[39m\u001b[32m\'test\'\u001b[39m\u001b[90m]\u001b[39m\u001b[90m.\u001b[39mforEach\u001b[90m(\u001b[39mregularFunc2\u001b[90m)\u001b[39m\u001b[90m;\u001b[39m\n' +
+    '   107 |\n' + '   108 |    \u001b[36mreturn\u001b[39m frames\u001b[90m;\u001b[39m\n' +
+    '   109 |\u001b[90m}\u001b[39m\u001b[90m;\u001b[39m\n' + '   110 |'
+];
--- test/test.js
+++ test/test.js
@@ -1,16 +1,20 @@
-var assert          = require('assert');
-var Promise         = require('pinkie-promise');
-var renderers       = require('..').renderers;
-var createFrames    = require('./data/create-frames');
-var expectedNoColor = require('./data/expected-no-color');
+// NOTE: enable chalk before any dependency is loaded
+require('chalk').enabled = true;
 
+var assert             = require('assert');
+var Promise            = require('pinkie-promise');
+var renderers          = require('..').renderers;
+var createFrames       = require('./data/create-frames');
+var expectedDefaultWin = require('./data/expected-default-win');
+var expectedNoColor    = require('./data/expected-no-color');
 
 it('Should create and render callsite records with "default" renderer', function () {
-    createFrames(true).forEach(function (frame) {
-        console.log(frame);
-        console.log();
-        console.log();
-    });
+    assert.deepEqual(createFrames(true), expectedDefaultWin);
+
+    return Promise.all(createFrames(false))
+        .then(function (rendered) {
+            assert.deepEqual(rendered, expectedDefaultWin);
+        });
 });
 
 it('Should create and render callsite records with "noColor" renderer', function () {
