94aad39ae81ba9a02bc1d401970e62861e61d7f4
inikulin <ifaaan@gmail.com>
2016-02-15 13:09:56 +0300

    Fix HTML tests

--- test/data/expected-html/index.js
+++ test/data/expected-html/index.js
@@ -1,6 +1,18 @@
-var fs = require('fs');
+var fs   = require('fs');
+var join = require('path').join;
+
+var getRecordsFile = join(__dirname, '../get-records.js');
+var testFile       = join(__dirname, '../../test.js');
+
 
 module.exports = [];
 
-for (var i = 0; i < 6; i++)
-    module.exports.push(fs.readFileSync('test/data/expected-html/' + i + '.html').toString().trim());
+for (var i = 0; i < 6; i++) {
+    var expectedFrame = fs.readFileSync('test/data/expected-html/' + i + '.html')
+        .toString()
+        .trim()
+        .replace(/\{\{\{get-records-file\}\}\}/g, getRecordsFile)
+        .replace(/\{\{\{test-file\}\}\}/g, testFile);
+
+    module.exports.push(expectedFrame);
+}
